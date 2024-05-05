5466f0ffa827e73c78771c6b6dbb3f1f1716c14f
ekocak <ekremkocak@msn.com>
2024-05-05 23:18:42 +0300

    Change hello world animation

--- app/src/main/java/com/ekremkocak/alzheimer/util/Constants.kt
+++ app/src/main/java/com/ekremkocak/alzheimer/util/Constants.kt
@@ -5,8 +5,10 @@ object Constants {
     const val DATABASE_NAME = "location.db"
     const val MIN_RANGE_CHANGE = -1f
     const val CHANNEL_ID = "LocationChannel"
+    const val CHANNEL_NAME = "LocationName"
     const val PREFS_NAME = "AppPrefs"
     const val TRACKING_ENABLED = "trackingEnabled"
+    const val CAMERA_SLIDE_EFFECT_DURATION = 4000
 }
 
 
